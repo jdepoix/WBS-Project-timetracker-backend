e79f343a469686179f7335df7b2e26d76be12680
Jonas Depoix <jdepoix@seibert-media.net>
2016-06-12 13:44:10 +0200

    project model view set implemented

--- wbs_timetracker/api/urls.py
+++ wbs_timetracker/api/urls.py
@@ -1,8 +1,16 @@
 from django.conf.urls import url
 
 from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
 
+from api.projects.views import ProjectsModelViewSet
+
+
+router = DefaultRouter()
+router.register(r'projects', ProjectsModelViewSet)
 
 urlpatterns = [
     url(r'^login', views.obtain_auth_token),
 ]
+
+urlpatterns += router.urls
