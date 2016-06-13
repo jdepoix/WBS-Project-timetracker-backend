dc2e3d4ea0b8449024b110eb610109694865b6a2
Jonas Depoix <jdepoix@seibert-media.net>
2016-06-13 20:48:50 +0200

    workpackage relation to bookings endpoint added

--- wbs_timetracker/api/bookings/serializers.py
+++ wbs_timetracker/api/bookings/serializers.py
@@ -1,10 +1,9 @@
-from rest_framework import serializers
-
 from core.api.serializers import BaseModelSerializer, ZeroTimeDateTimeField
 
 from data.legacy.project.models import WorkEffort
 
 from api.projects.serializers import SubProjectHyperlinkedRelatedField
+from api.workpackages.serializers import WorkpackageHyperlinkedRelatedField
 
 
 class BookingsHyperlinkedRelatedField(SubProjectHyperlinkedRelatedField):
@@ -16,10 +15,10 @@ class BookingsHyperlinkedRelatedField(SubProjectHyperlinkedRelatedField):
 
 
 class WorkEffortSerializer(BaseModelSerializer):
-    #TODO add link to workpackage
     self = BookingsHyperlinkedRelatedField(read_only=True, source='*')
+    workpackage = WorkpackageHyperlinkedRelatedField()
     date = ZeroTimeDateTimeField(source='rec_date')
 
     class Meta:
         model = WorkEffort
-        fields = ['effort', 'description', 'date', 'self']
+        fields = ('effort', 'description', 'date', 'self', 'workpackage',)
--- wbs_timetracker/api/projects/serializers.py
+++ wbs_timetracker/api/projects/serializers.py
@@ -19,10 +19,13 @@ class SubProjectHyperlinkedRelatedField(serializers.HyperlinkedRelatedField):
         return reverse(view_name, kwargs=url_kwargs, request=request, format=format)
 
     def get_object(self, view_name, view_args, view_kwargs):
-        return self.model.objects.using(
-            view_kwargs.get('project_id')
-        ).get(
+        return self.get_queryset().get(
             view_kwargs.get('pk')
+        )
+
+    def get_queryset(self):
+        self.model.objects.using(
+            self.context.get('request').parser_context.get('kwargs').get('project_id')
         )
 
     @abstractproperty
