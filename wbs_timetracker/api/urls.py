d77a7fd4340904902c9fb8d6a13ea0261151c823
Jonas Depoix <jdepoix@seibert-media.net>
2016-06-13 16:39:42 +0200

    router for workpackage endpoints added to urls.py

--- wbs_timetracker/api/urls.py
+++ wbs_timetracker/api/urls.py
@@ -13,8 +13,12 @@ projects_router.register(r'projects', ProjectsModelViewSet)
 bookings_router = DefaultRouter()
 bookings_router.register(r'bookings', BookingsModelViewSet, base_name='workeffort')
 
+workpackage_router = DefaultRouter()
+workpackage_router.register(r'workpackages', BookingsModelViewSet, base_name='workpackage')
+
 urlpatterns = [
     url(r'^login', views.obtain_auth_token),
     url(r'^', include(projects_router.urls)),
     url(r'^projects/(?P<project_id>\d+)/', include(bookings_router.urls)),
+    url(r'^projects/(?P<project_id>\d+)/', include(workpackage_router.urls)),
 ]
