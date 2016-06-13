0c1e2933f725ae188f5315071b29d68158f00126
Jonas Depoix <jdepoix@seibert-media.net>
2016-06-13 14:22:14 +0200

    bookings urls mapped

--- wbs_timetracker/api/urls.py
+++ wbs_timetracker/api/urls.py
@@ -1,16 +1,20 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 from api.projects.views import ProjectsModelViewSet
+from api.bookings.views import BookingsModelViewSet
 
 
-router = DefaultRouter()
-router.register(r'projects', ProjectsModelViewSet)
+projects_router = DefaultRouter()
+projects_router.register(r'projects', ProjectsModelViewSet)
+
+bookings_router = DefaultRouter()
+bookings_router.register(r'bookings', BookingsModelViewSet, base_name='workeffort')
 
 urlpatterns = [
     url(r'^login', views.obtain_auth_token),
+    url(r'^', include(projects_router.urls)),
+    url(r'^projects/(?P<project_id>\d+)/', include(bookings_router.urls)),
 ]
-
-urlpatterns += router.urls
