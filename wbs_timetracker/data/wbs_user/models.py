d141f22f2cfce3e67ee5dd2fc82c445d80f25407
Jonas Depoix <jdepoix@seibert-media.net>
2016-06-21 17:20:04 +0200

    booking session model added

--- wbs_timetracker/data/wbs_user/models.py
+++ wbs_timetracker/data/wbs_user/models.py
@@ -3,13 +3,33 @@ from django.db import models
 from django.db.models.signals import post_save
 
 from data.legacy.id_wbs.models import DbIdentifier
+from data.legacy.project.models import Workpackage
+
+
+class BookingSession(models.Model):
+    """
+    Represents a single booking session for a specific user. Each user can only have one booking session at a time
+    """
+    # since workpackages are stored in a different DB, a ForeignKey can't be used here
+    workpackage_id = models.IntegerField()
+    db = models.ForeignKey(DbIdentifier)
+    user = models.OneToOneField('WbsUser', related_name='booking_session', on_delete=models.DO_NOTHING)
+    start_time = models.DateTimeField()
+
+    @property
+    def workpackage(self):
+        return Workpackage.object.using(str(self.db.id)).get(pk=self.workpackage_id)
 
 
 class WbsUser(models.Model):
+    """
+    extends the django user model and adds relations to the projects the given user is participating in
+    """
     user = models.OneToOneField(User, related_name='wbs_user', on_delete=models.CASCADE)
     projects = models.ManyToManyField(DbIdentifier, related_name='wbs_users')
-    # TODO rework booking model
-    start_current_booking_session = models.DateTimeField(null=True, default=None)
+
+    def __unicode__(self):
+        return self.user.username
 
     @staticmethod
     def create_wbs_user(sender, instance, created, **kwargs):
