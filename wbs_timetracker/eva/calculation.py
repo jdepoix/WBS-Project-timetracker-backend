5ce7436184884bd4645482dafa685d3330482ea6
Jonas Depoix <jdepoix@seibert-media.net>
2016-06-27 17:10:17 +0200

    calculating toplevel workpackages also implemented

--- wbs_timetracker/eva/calculation.py
+++ wbs_timetracker/eva/calculation.py
@@ -4,7 +4,7 @@ from core.queryset_cache import QuerysetCache
 
 from data.legacy.project.models import Workpackage, WorkEffort, Employees
 
-class CalculatabelWorkpackage(object):
+class CalculatableWorkpackage(object):
     """
     Wraps a Workpackage and adds additional functionality needed to calculate the earned value analysis values
     """
@@ -43,8 +43,20 @@ class CalculatabelWorkpackage(object):
         """
         Calculates a toplevel workpackage
         """
-        # TODO implement
-        pass
+        self.workpackage.etc = 0
+        self.workpackage.etc_costs = 0
+        self.workpackage.ac = 0
+        self.workpackage.ac_costs = 0
+        self.workpackage.eac = 0
+        self.workpackage.ev = 0
+
+        for child in self._get_children():
+            self.workpackage.etc += child.etc
+            self.workpackage.etc_costs += child.etc_costs
+            self.workpackage.ac += child.ac
+            self.workpackage.ac_costs += child.ac_costs
+            self.workpackage.eac += child.eac
+            self.workpackage.ev += child.ev
 
     def _get_completion_status(self):
         """
@@ -120,7 +132,7 @@ class CalculatabelWorkpackage(object):
 
         :rtype: list<WorkEffort>
         """
-        return self.calculation_manager.get_work_efforts_by_workpackage(self.workpackage)
+        return self.calculation_manager.get_cached_work_efforts_by_workpackage(self.workpackage)
 
     def _get_employee_by_work_effort(self, work_effort):
         """
@@ -131,7 +143,16 @@ class CalculatabelWorkpackage(object):
         :return: the employee belonging to the given WorkEffort
         :rtype: Employees
         """
-        return self.calculation_manager.get_employee_by_work_effort(work_effort)
+        return self.calculation_manager.get_cached_employee_by_work_effort(work_effort)
+
+    def _get_children(self):
+        """
+        Returns the child workpackages for a this workpackage, from the workpackages cached by the caluclation manager
+
+        :return: list of child workpackages
+        :rtype: list<Workpackage>
+        """
+        return self.calculation_manager.get_cached_worpackage_children(self.workpackage)
 
 
 class EVACalculationManager(object):
@@ -150,12 +171,12 @@ class EVACalculationManager(object):
         self.calculated_workpackages = []
         """holds the workpackages which have been changed"""
 
-        project_id = self.initial_workpackage._state.db
+        self.project_id = self.initial_workpackage._state.db
 
         # setup QuerysetCache
-        self.workpackage_cache = QuerysetCache(Workpackage.objects.using(project_id).all())
-        self.work_effort_cache = QuerysetCache(WorkEffort.objects.using(project_id).all())
-        self.employee_cache = QuerysetCache(Employees.objects.using(project_id).all())
+        self.workpackage_cache = QuerysetCache(Workpackage.objects.using(self.project_id).all())
+        self.work_effort_cache = QuerysetCache(WorkEffort.objects.using(self.project_id).all())
+        self.employee_cache = QuerysetCache(Employees.objects.using(self.project_id).all())
 
         # setup attribute maps
         self.workpackage_cache.map_by_attribute('id', unique=True)
@@ -163,7 +184,7 @@ class EVACalculationManager(object):
         self.work_effort_cache.map_by_attribute('workpackage_id')
         self.employee_cache.map_by_attribute('id', unique=True)
 
-    def get_work_efforts_by_workpackage(self, workpackage):
+    def get_cached_work_efforts_by_workpackage(self, workpackage):
         """
         Returns the WorkEfforts for a workpackage from the cached WorkEfforts
 
@@ -171,9 +192,9 @@ class EVACalculationManager(object):
         :return: list of WorkEfforts
         :rtype: list<WorkEffort>
         """
-        return self.work_effort_cache.workpackage_id_map.get(workpackage.id)
+        return self.work_effort_cache.workpackage_id_map.get(workpackage.id, [])
 
-    def get_employee_by_work_effort(self, work_effort):
+    def get_cached_employee_by_work_effort(self, work_effort):
         """
         Returns a employee who did make the given WorkEffort
 
@@ -182,7 +203,18 @@ class EVACalculationManager(object):
         :return: the employee belonging to the given WorkEffort
         :rtype: Employees
         """
-        return self.employee_cache.id_map.get(work_effort.employee.id)
+        return self.employee_cache.id_map.get(work_effort.employee_id)
+
+    def get_cached_worpackage_children(self, workpackage):
+        """
+        Returns the child workpackages for a given toplevel workpackage, from the cached Workpackages
+
+        :param workpackage: a toplevel workpackage the children should be retrieved for
+        :type workpackage: Workpackage
+        :return: list of child workpackages
+        :rtype: list<Workpackage>
+        """
+        return self.workpackage_cache.parent_id_map.get(workpackage.id, [])
 
     def calculate(self):
         """
@@ -198,7 +230,7 @@ class EVACalculationManager(object):
         :param initial_workpackage: the initial workpackage
         :type workpackage: Workpackage
         """
-        CalculatabelWorkpackage(initial_workpackage).calculate()
+        CalculatableWorkpackage(initial_workpackage, self).calculate()
         self.calculated_workpackages.append(initial_workpackage)
 
         parent = self.workpackage_cache.id_map.get(initial_workpackage.parent_id)
@@ -211,6 +243,6 @@ class EVACalculationManager(object):
         Saves the workpackages which have been calculated. This is done in an atomic transaction, to avoid multiple
         requests.
         """
-        with atomic():
+        with atomic(using=self.project_id):
             for workpackage in self.calculated_workpackages:
                 workpackage.save()
