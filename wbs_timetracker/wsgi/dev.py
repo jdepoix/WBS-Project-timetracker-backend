c2d6e52780daa2d7bd1be01804f759b61f3eeb46
Jonas Depoix <jdepoix@seibert-media.net>
2016-06-28 15:55:07 +0200

    python path append added to wsgi files

--- wbs_timetracker/wsgi/dev.py
+++ wbs_timetracker/wsgi/dev.py
@@ -8,8 +8,14 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+
+sys.path.append(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wbs_timetracker.settings.dev")
 
